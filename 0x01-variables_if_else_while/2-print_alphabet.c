6c3335511fa49bbd933ef5ea2ad0ac91958a5181
Ruba-Salih <rubasalih900@gmail.com>
2023-10-09 06:28:56 -0700

    alx

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+
 /**
  * main - Entry point
  *
@@ -13,7 +12,8 @@ int main(void)
 
 	for (low = 'a'; low <= 'z'; low++)
 		putchar(low);
-		putchar('\n');
+
+	putchar('\n');
 
 	return (0);
 }
