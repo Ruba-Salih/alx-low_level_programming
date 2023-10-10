a7af2a472471a713b3153bfec29fe867320061e3
Ruba-Salih <rubasalih900@gmail.com>
2023-10-10 08:15:15 -0700

    alx

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_last_digit - Entry point
+ * @d: an int
+ * Return: Always 0 (Success)
+ */
+int print_last_digit(int d)
+{
+	return (d % 10);
+}
