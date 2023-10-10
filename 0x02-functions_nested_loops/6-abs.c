e3e86d25d630af8ba342701de65978981166bb04
Ruba-Salih <rubasalih900@gmail.com>
2023-10-10 08:10:41 -0700

    alx

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _abs - Entry point
+ * @a: an int
+ * Return: Always 0 (Success)
+ */
+int _abs(int a)
+{
+	if (a > 0)
+		return (a);
+	else
+		return (-a);
+}
