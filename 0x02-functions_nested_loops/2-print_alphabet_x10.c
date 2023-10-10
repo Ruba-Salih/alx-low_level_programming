d5c31e218f9b6420e3ecbf6497e665a2a60b04f6
Ruba-Salih <rubasalih900@gmail.com>
2023-10-10 07:32:18 -0700

    alx

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-#include "main_h"
+#include "maini.h"
 /**
- * print_alphabet - Entry point
+ * print_alphabet_x10 - Entry point
  *
  * Return: Always 0 (Success)
  */
-void print_alphabet(void)
+void print_alphabet_x10(void)
 {
         char alp;
 	int num;
