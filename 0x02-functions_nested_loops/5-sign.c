c223334b04dc16be6ffc7cd60a4ba90a8e8d72a2
Ruba-Salih <rubasalih900@gmail.com>
2023-10-10 08:04:32 -0700

    alx

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_sign - Entry point
+ * @n: an int
+ * Return: Always 0 (Success)
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		return (1);
+		_putchar('+');
+	}
+	else if (n == 0)
+	{
+		return (0);
+		_putchar('0');
+	}
+	else
+	{
+		return (-1);
+		_putchar('-');
+	}
+}
