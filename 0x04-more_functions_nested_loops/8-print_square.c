bab2b935fc1388180367d8000b45f8512247f310
Ruba-Salih <rubasalih900@gmail.com>
2023-10-12 10:48:12 -0700

    alx

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_square - Entry point
+ * @size: int
+ * Return: Always 0 (Success)
+ */
+void print_square(int size)
+{
+	int i, j;
+
+	if (size > 0)
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+				_putchar('#');
+			_putchar('\n');
+		}
+	}
+	_putchar('\n');
+}
