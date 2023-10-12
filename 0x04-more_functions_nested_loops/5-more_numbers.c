48c731745edc6b7f470cb0c68f483b9874851057
Ruba-Salih <rubasalih900@gmail.com>
2023-10-12 09:39:52 -0700

    alx

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * more_numbers - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+void more_numbers(void)
+{
+	int i, c;
+
+	for (i = 0; i < 10 ; i++)
+	{
+		for (num = 0; num <= 14; num++)
+		{
+			if (num > 9)
+				_putchar((num / 10) + '0');
+			_putchar((num % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
