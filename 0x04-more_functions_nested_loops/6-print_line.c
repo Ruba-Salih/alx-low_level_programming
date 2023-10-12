1eca5b95980f4d1f60093075e93654a95014014e
Ruba-Salih <rubasalih900@gmail.com>
2023-10-12 10:17:04 -0700

    alx

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -7,11 +7,14 @@
  */
 void print_line(int n)
 {
-	if (n <= 0)
-		_putchar('\n');
-	else
+	if (n > 0)
 	{
 		while (n != 0)
-			_putchar('_');
+		{
+                        _putchar('_');
+			n--;
+		}
 	}
+	else
+		_putchar('\n');
 }
