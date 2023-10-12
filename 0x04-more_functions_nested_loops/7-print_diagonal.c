5c2e632c5f39220091916867a2bd5914b1463763
Ruba-Salih <rubasalih900@gmail.com>
2023-10-12 10:35:49 -0700

    alx

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -7,15 +7,22 @@
  */
 void print_diagonal(int n)
 {
+	int len, space;
+
 	if (n > 0)
 	{
-		while (n != 0)
+		for (len = 0; len < n; len++)
 		{
+			for (space = 0; space < len; space++)
+				_putchar(' ');
 			_putchar('\\');
-			_putchar(' ');
+
+			if (len == n - 1)
+				continue;
+
 			_putchar('\n');
-			n--;
 		}
 	}
+
 	_putchar('\n');
 }
