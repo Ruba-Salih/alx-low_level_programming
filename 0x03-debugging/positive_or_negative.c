90a721c4f78d54934fbf1e4e7b25b5c47e6ea222
Ruba-Salih <rubasalih900@gmail.com>
2023-10-12 05:27:31 -0700

    alx

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -7,15 +7,12 @@
  * Return: Always 0 (Success)
  */
 /* betty style doc for function main goes there */
-int positive_or_negative(int n)
+void positive_or_negative(int n)
 {
-        int n;
-
-        if (n > 0)
-                printf("%d is positive\n", n);
-        if (n == 0)
-                printf("%d is zero\n", n);
-        if (n < 0)
-                printf("%d is negative\n", n);
-        return (0);
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n == 0)
+		printf("%d is zero\n", n);
+	else
+		printf("%d is negative\n", n);
 }
