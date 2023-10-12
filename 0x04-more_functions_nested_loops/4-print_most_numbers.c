28fffa523ece5e3a9bcdbca6433d23b0325777b9
Ruba-Salih <rubasalih900@gmail.com>
2023-10-12 09:22:31 -0700

    alx

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_most_numbers - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_most_numbers(void)
+{
+	int c;
+
+	for (c = '0'; c < '0'; c++)
+	{
+		if (c != '2' && c != '4')
+			_putchar(c);
+	}
+	_putchar('\n');
+}
