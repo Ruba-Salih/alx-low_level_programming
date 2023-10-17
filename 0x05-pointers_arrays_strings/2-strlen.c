7b62303fe9492ee83e6c8f69bee7890bfa2d58ba
Ruba-Salih <rubasalih900@gmail.com>
2023-10-17 13:36:39 -0700

    alx

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strlen - Entry point
+ * @s : int
+ * Return: Always 0 (Success)
+ */
+int _strlen(char *s)
+{
+	int i, c;
+
+	c = 0;
+	for (i = 0; i < '\0'; i++)
+	{
+		s[i];
+	}
+
+	return (i);
+}
