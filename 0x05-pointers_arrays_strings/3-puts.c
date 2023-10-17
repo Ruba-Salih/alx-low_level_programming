659117f3715c9bd3bcc08f43757d27412ad2e08d
Ruba-Salih <rubasalih900@gmail.com>
2023-10-17 14:12:41 -0700

    alx

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _puts - Entry point
+ * @str: int
+ * Return: Always 0 (Success)
+ */
+void _puts(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\n')
+	{
+		printf("%c", str[i]);
+	}
+	printf("\n");
+}
