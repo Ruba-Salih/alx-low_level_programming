b04efc594c26dc3498fa787e1ad6bdc8286e67b9
Ruba-Salih <rubasalih900@gmail.com>
2023-10-19 12:04:59 -0700

    alx

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * _strcat - Entry point
+ * @dest: int
+ * @src: int
+ * Return: Always 0 (Success)
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i, len;
+
+	for (i = 0; dest[i] != '\0'; i++);
+
+	for (len = 0; src[len] != '\0'; len++)
+	{
+		dest[i + len] = src[len];
+	}
+	dest[i + len] = '\0';
+	return dest;
+}
