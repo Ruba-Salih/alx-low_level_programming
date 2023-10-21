e115d86e683bafe4df25facadf3d415a65b175c8
Ruba-Salih <rubasalih900@gmail.com>
2023-10-21 12:47:12 -0700

    alx

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -13,10 +13,11 @@ void print_array(int *a, int n)
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", a[i]);
-		if (i != n)
+		if (i + 1 < n)
 		{
 			printf(",");
 			printf(" ");
 		}
 	}
+	printf("\n");
 }
