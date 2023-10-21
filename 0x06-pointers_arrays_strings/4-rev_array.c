ed6eb1250e748269c1293fb4afad8a0224cf16e3
Ruba-Salih <rubasalih900@gmail.com>
2023-10-21 13:32:19 -0700

    alx

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * reverse_array - Entry point
+ * @a: int
+ * @n: int
+ * Return: Always 0 (Success)
+ */
+void reverse_array(int *a, int n)
+{
+	int i, tmp = 0;
+
+	int j = n;
+
+	for (i = 0; i < n; i++)
+	{
+		tmp = a[i];
+		a[i] = a[n];
+		a[n++] =  tmp;
+	}
+}
