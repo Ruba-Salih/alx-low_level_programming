c8318c8f426b224d9406fb101cc5ff686c956744
Ruba-Salih <rubasalih900@gmail.com>
2023-10-25 23:59:17 -0700

    alx

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _pow_recursion - Entry point
+ * @x: int
+ * @y: int
+ * Return: Always 0 (Success)
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y == 1)
+		return (x);
+	if (y < 0)
+		return (-1);
+	x *= _pow_recursion(x , y - 1);
+	return (x);
+}
