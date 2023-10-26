1a02fab6583ce593b79371f82fa6dd582c0a06c3
Ruba-Salih <rubasalih900@gmail.com>
2023-10-25 23:05:04 -0700

    alx

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,11 +1,11 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * _puts_recursion - Entry point
+ * _print_rev_recursion - Entry point
  * @s: char
  * Return: Always 0 (Success)
  */
-void _puts_recursion(char *s)
+void _print_rev_recursion(char *s)
 {
 	int i = 0;
 
@@ -14,6 +14,7 @@ void _puts_recursion(char *s)
 		printf("\n");
 		return;
 	}
-	printf("%c", s[i]);
+
 	_puts_recursion(s + 1);
+	printf("%c", s[i]);
 }
