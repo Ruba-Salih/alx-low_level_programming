5e05bd8279a8595381cff2a390c9ebfbc9d40353
Ruba-Salih <rubasalih900@gmail.com>
2023-10-27 19:14:26 -0700

    alx

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,8 +5,9 @@
  * @argv: int
  * Return: Always 0 (Success)
  */
-int main(int __attribute__((__unused__)) argc, char __attribute__((__unused__)) *argv[])
+int main(int argc, char __attribute__((__unused__)) *argv[])
 {
+	(void) argc;
 	printf("%s\n", argv[0]);
 	return (0);
 }
