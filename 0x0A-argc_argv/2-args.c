cdb8a4e20d860be037dda19fd9b2c60744f1134d
Ruba-Salih <rubasalih900@gmail.com>
2023-10-27 19:28:15 -0700

    alx

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * @argc: int
+ * @argv: int
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int i = 0;
+
+	while (i < argc)
+	{
+		printf("%s\n", argv[i]);
+		i++;
+	}
+	return (0);
+}
