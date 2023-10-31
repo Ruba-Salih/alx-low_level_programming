d0e29475ec6ccac5a65fc60699f04f309afd907e
Ruba-Salih <rubasalih900@gmail.com>
2023-10-31 08:28:58 -0700

    alx

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -9,21 +9,23 @@
  */
 int **alloc_grid(int width, int height)
 {
+	int **array, i = 0, j = 0;
+
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	int **ar;
-	int i, j;
-
-	ar = malloc(sizeof(int) * (width * height));
-
-	for (i = 0; i < width; i++)
+	array = (int **)malloc(sizeof(int *) * height);
+	if (array == NULL)
+		return (NULL);
+	for (; i < height; i++)
 	{
-		for (j = 0; j < height; j++)
-		{
-			ar[i][j] = 0;
-		}
+		array[i] = (int *)malloc(sizeof(int) * width);
+		if (array[i] == NULL)
+			return (NULL);
 	}
 
-	return (ar);
+	for (i = 0; i < height; i++)
+		for (; j < width; j++)
+			array[i][j] = 0;
+	return (array);
 }
