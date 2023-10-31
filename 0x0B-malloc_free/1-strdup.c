b5ddea05fee6370298cab6c37989eee42bcdf1d9
Ruba-Salih <rubasalih900@gmail.com>
2023-10-30 23:03:28 -0700

    alx

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * _strdup - Entry point
+ * @str: int
+ * Return: Always 0 (Success)
+ */
+char *_strdup(char *str)
+{
+	char *ar;
+	int i, len;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		len = strlen(str);
+		ar = malloc(sizeof(char) * len);
+
+		if (ar != NULL)
+		{
+			for (i = 0; i < len; i++)
+			{
+				ar[i] = str[i];
+			}
+		}
+		else
+			return (NULL);
+	}
+	return (ar);
+}
