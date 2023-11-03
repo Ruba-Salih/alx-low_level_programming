949b77636e06bb32cc072b957e26e15a877a48be
Ruba-Salih <rubasalih900@gmail.com>
2023-11-03 06:49:24 -0700

    alx

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -15,7 +15,7 @@ int *array_range(int min, int max)
 		return (NULL);
 
 	sum = max - min + 1;
-	ptr = malloc(sizeof(ptr) * sum);
+	ptr = malloc(sizeof(int) * sum);
 
 	if (ptr == NULL)
 		return (NULL);
