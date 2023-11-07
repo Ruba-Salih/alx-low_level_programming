26e24157bb247213ac5dbd5eba1f88fdd3455cc1
Ruba-Salih <rubasalih900@gmail.com>
2023-11-07 07:07:01 -0800

    alx

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,7 +1,10 @@
-#include <stdio.h> 
-int main() 
-{ 	
-	printf("%s\n", __FILE__); 
-	return 0; 
-} 
-
+#include <stdio.h>
+/**
+ * main - prints the name of file
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
