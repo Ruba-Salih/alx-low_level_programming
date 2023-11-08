4328a2e2565bb8bd92bac93d45d1543272bfe951
Ruba-Salih <rubasalih900@gmail.com>
2023-11-08 09:58:49 -0800

    alx

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+ * array_iterator - Entry point
+ * @array: int
+ * @size: int
+ * @action: int
+ * Return: Always 0 (Success)
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i;
+
+	if (array == NULL || action == NULL)
+		return;
+
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
