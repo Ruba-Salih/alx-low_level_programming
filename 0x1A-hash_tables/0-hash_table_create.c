a4b6bfc5e17cd10e570cb434c1267b14c875ff80
Ruba-salih <rubasalih900@gmail.com>
2024-02-04 05:27:28 -0800

    alx

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,29 @@
+#include "hash_tables.h"
+/**
+ * hash_table_create - Node of a hash table
+ * @size: A pointer to the next node of the List
+ * Return: table
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	unsigned long int i;
+
+	hash_table_t * table = malloc(sizeof(hash_table_t));
+	if (new_tab == NULL)
+		return (NULL);
+
+	table->size = size;
+	table->array = calloc(table->size, sizeof(hash_node_t));
+	if (table->array == NULL)
+	{
+		free(table);
+		return (NULL);
+	}
+
+	for (i = 0, table->size, i++)
+	{
+		table->array[i] = NULL;
+	}
+	return (table);
+}
