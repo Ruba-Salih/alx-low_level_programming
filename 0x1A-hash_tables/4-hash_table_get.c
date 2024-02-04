1155374374d4390a7e84c748b36568f93ae1318b
Ruba-salih <rubasalih900@gmail.com>
2024-02-04 10:32:29 -0800

    alx

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,29 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - Node of a hash table
+ * @ht: A pointer to the next node of the List
+ * @key: A pointer to the next node of the List
+ * Return: table
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index;
+	hash_node_t *node;
+
+	if (key == NULL || ht == NULL)
+		return (NULL);
+
+	index = key_index((unsigned char *) key, ht->size);
+
+	if (ht->array[index] == NULL)
+		return (NULL);
+	node = ht->array[index];
+	while (node != NULL)
+	{
+		if (strcmp(node->key, key))
+			return (node->value);
+		node = node->next;
+	}
+	return (NULL);
+}
