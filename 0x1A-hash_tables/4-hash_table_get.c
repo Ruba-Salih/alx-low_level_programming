27cdb5bf918228132e98c054af8c5d32c0f4ff69
Ruba-salih <rubasalih900@gmail.com>
2024-02-04 11:34:32 -0800

    alx

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -23,7 +23,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	node = ht->array[index];
 	while (node != NULL)
 	{
-		if (strcmp(node->key, key)== 0)
+		if (strcmp(node->key, key) == 0)
 			return (node->value);
 		node = node->next;
 	}
